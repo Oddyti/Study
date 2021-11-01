7c2913f187e37f43f98f6c261524654ffb516836
oddyti <2067438609@qq.com>
2021-11-01 23:00:04 +0800

    更新了模版的中文编号更换成了zhnum宏包

--- DSP/src/myPlot2.m
+++ DSP/src/myPlot2.m
@@ -1,4 +1,10 @@
-function y = myPlot(xt, xn, t, n, Name)
+% 生成序列并绘图
+function y = myPlot2(T, N, Name)
+    f = 50;
+    t = 0:0.001:0.1;
+    n = 0:1:N-1;
+    xt = sin(2*pi*f*t);
+    xn = sin(2*pi*f*T*n);
     h1 = figure(1);
     set(gcf,'outerposition',get(0,'screensize'));
         subplot(2, 1, 1);
@@ -8,7 +14,7 @@ function y = myPlot(xt, xn, t, n, Name)
                 set(gca,'FontSize',16);
         subplot(2, 1, 2);
             stem(n, xn, 'filled');
-                title('采样','FontSize',20);
+                title('采样后的序列','FontSize',20);
                 xlabel('n');
                 set(gca,'FontSize',16);
     saveas(h1, [Name,'_1'],'png');
@@ -16,57 +22,53 @@ function y = myPlot(xt, xn, t, n, Name)
     h2 = figure(2);
     set(gcf,'outerposition',get(0,'screensize'));
     % 实部
-    subplot(2, 4, 1);
+    subplot(2, 2, 1);
         stem(n, real(xn), 'filled');
         title('实部','FontSize',20);
         xlabel('n');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 1, 1);
     % 虚部
-    subplot(2, 4, 2);
+    subplot(2, 2, 2);
         stem(n, imag(xn), 'filled');
         title('虚部','FontSize',20);
         xlabel('n');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 1, 2);
     % 模
-    subplot(2, 4, 3);
+    subplot(2, 2, 3);
         stem(n, abs(xn), 'filled');
         title('模','FontSize',20);
         xlabel('n');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 1, 3);
     % 相角
-    subplot(2, 4, 4);
+    subplot(2, 2, 4);
         stem(n, (180/pi)*angle(xn), 'filled');
         title('相角','FontSize',20);
         xlabel('n');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 1, 4);
+    saveas(h2, [Name,'_2'],'png');
     % 绘制幅度谱、频谱实部、频谱虚部图
     % 求DFT
     % Xk = myDFT(x, N);
     Xk = fft(xn);
+    h3 = figure(3);
+    set(gcf,'outerposition',get(0,'screensize'));
     % 幅度谱
-    subplot(2, 4, 5);
+    subplot(3, 1, 1);
         stem(n, abs(Xk), 'filled');
-        title('幅度谱','FontSize',20);
+        title('DFT幅度谱','FontSize',20);
         xlabel('k');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 2, 1);
     % 频谱实部
-    subplot(2, 4, 6);
+    subplot(3, 1, 2);
         stem(n, real(Xk), 'filled');
-        title('频谱实部','FontSize',20);
+        title('DFT实部','FontSize',20);
         xlabel('k');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 2, 2);
     % 频谱虚部
-    subplot(2, 4, 7);
+    subplot(3, 1, 3);
         stem(n, imag(Xk), 'filled');
-        title('频谱虚部','FontSize',20);
+        title('DFT虚部','FontSize',20);
         xlabel('k');
     set(gca,'FontSize',16);
-    RemoveSubplotWhiteArea(gca, 2, 4, 2, 3);
-    saveas(h2, [Name,'_2'],'png');
+    saveas(h3, [Name,'_3'],'png');
 end
