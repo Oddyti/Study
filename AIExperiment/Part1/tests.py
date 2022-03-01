316450ea8c2f5bb775d6c541cc54dcdbdc287891
Oddyti <2067438609@qq.com>
2022-03-01 15:11:59 +0800

    单词表

--- AIExperiment/Part1/tests.py
+++ AIExperiment/Part1/tests.py
@@ -1,6 +1,7 @@
 import kNN
 group, lables = kNN.createDataSet()
-print(kNN.classify0([0, 0], group, lables, k = 3))
-print(kNN.classify0([0.8, 0.7], group, lables, k = 3))
+print('分类结果')
+print('[0, 0] %c' %(kNN.classify0([0, 0], group, lables, k = 3)))
+print('[0.8, 0.7] %c' %(kNN.classify0([0.8, 0.7], group, lables, k = 3)))
 
 
