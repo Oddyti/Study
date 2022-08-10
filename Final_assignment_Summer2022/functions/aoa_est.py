ef50ffd66e971cbed0cc8685ec767c5c0a6e2548
oddyti <2067438609@qq.com>
2022-08-10 18:53:15 +0800

    uiuc-4

--- Final_assignment_Summer2022/functions/aoa_est.py
+++ Final_assignment_Summer2022/functions/aoa_est.py
@@ -10,17 +10,16 @@ def aoa_est(mixtures, M, N, space, sr, c):
 
     theta = np.arange(0, np.pi, np.pi/200)
 
-    N = mixtures.shape[-1]
-    trans = N/sr
+    trans = mixtures.shape[-1]/sr
     # trans the index of fft to the real fi
 
     mixtures_fft = fft(mixtures)
 
+    # set the fi with step=100
     fi = np.arange(0, 8500, 100)
-    # set the fi with step=10Hz
 
-    ofunction = np.zeros(200)
     # save the objective function result
+    ofunction = np.zeros(200)
 
     for i in range(200):
         sum_1 = 0
@@ -32,18 +31,21 @@ def aoa_est(mixtures, M, N, space, sr, c):
                 sum_2 += (e * mixtures_fft[k, fi_i])
             sum_1 += abs(sum_2)
         ofunction[i] = sum_1
-
+    
     # Estimated AoA as per Eq 1 in assignmnt doc
-
     # Save estimated AoA in this variable and return in the function. For N multiple sound sources est_aoa is a Nx1 dimension vector
-    max = sg.argrelmax(ofunction)
-    for i in max:
-        print(theta[i])
     est_aoa = np.zeros((1, N))
+    if N == 1:
+        index = np.argmax(ofunction)
+        est_aoa = theta[index]
+    else:
+        est_aoa = theta[sg.argrelmax(ofunction)[0]]
 
+    # Plot the variation of objective function with thetas. Add the plot in your assignment report.
     plt.figure
     plt.plot(theta, ofunction)
-    plt.show
-    # Plot the variation of objective function with thetas. Add the plot in your assignment report.
+    plt.xlabel("theta")
+    plt.ylabel("argmax")
+    plt.show()
 
     return est_aoa
